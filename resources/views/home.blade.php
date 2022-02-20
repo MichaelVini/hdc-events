6aa51f3fc2069e99a09643c1eb5da61ce0aa8b6c
Michael Vinicius <michaelvini62@gmail.com>
2022-02-20 20:54:28 -0300

    create home events

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,27 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>Estamos na página inicial!</h1>
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="serch" class="form-control" placeholder="Pesquisar Evento">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Pŕoximos Eventos</h2>
+    <p>Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach ($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/event-banner.jpg" alt="">
+                <div class="card-body">
+                    <p class="card-date">10/09/2022</p>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants">X participantes</p>
+                    <a href="" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
