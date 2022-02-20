3e4570a787fd2e5153272bbcff453b6df2fce7b7
Michael Vinicius <michaelvini62@gmail.com>
2022-02-20 20:52:36 -0300

    create store function

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Event;
 class EventController extends Controller
 {
     public function index() {
-        return view('home');
+        $events = Event::all();
+
+        return view('home', ['events' => $events]);
     }
 
     public function create() {
         return view('events.create');
+    }
+
+    public function store(Request $request) {
+        $event = new Event();
+        
+        $event->title = $request->title;
+        $event->description = $request->description;
+        $event->city = $request->city;
+        $event->private = $request->private;
+
+        $event->save();
+
+        return redirect('/')->with('message', 'Evento criado com sucesso!');
     }
 }
