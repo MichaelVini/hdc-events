cf0a14ad8245b0df84f814e9a9a5f9e588ceed50
Michael Vinicius <michaelvini62@gmail.com>
2022-02-20 22:16:27 -0300

    add new route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/eventos/create', [EventController::class, 'create']);
+Route::post('/eventos', [EventController::class, 'store']);
