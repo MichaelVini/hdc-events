af5a757313f7791c93aabb308bc5bdc1c3b96093
Michael Vinicius <michaelvini62@gmail.com>
2022-03-05 18:06:24 -0300

    insert feature to search event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,9 +7,19 @@ use App\Models\Event;
 class EventController extends Controller
 {
     public function index() {
-        $events = Event::all();
 
-        return view('home', ['events' => $events]);
+        $search = request('search');
+
+        if (!empty($search)) {
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])
+            ->get();
+        } else {
+            $events = Event::all();
+        }
+
+        return view('home', ['events' => $events, 'search' => $search]);
     }
 
     public function create() {
