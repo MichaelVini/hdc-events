91eed29cdca2043921b17f0feb1862ae670c318a
Michael Vinicius <michaelvini62@gmail.com>
2022-03-05 16:59:45 -0300

    add input data and show on home

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -12,6 +12,10 @@
             <input type="text" class="form-control" name="title" id="title" placeholder="Nome do evento">
          </div>
          <div class="form-group">
+            <label for="date">Data do evento:</label>
+            <input type="date" class="form-control" name="date" id="date">
+         </div>
+         <div class="form-group">
             <label for="image">Imagem do evento: </label>
             <input type="file" class="form-control-file" name="image" id="image" placeholder="Imagem do evento">
          </div>
@@ -29,6 +33,24 @@
          <div class="form-group">
             <label for="description">Descrição</label>
             <textarea name="description" id="description" class="form-control" cols="30" rows="10"></textarea>
+         </div>
+         <div class="form-group">
+            <label for="items">Adicione itens de infraestrutura</label>
+            <div class="form-group">
+               <input type="checkbox" name="items[]" id="Cadeiras" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+               <input type="checkbox" name="items[]" id="Palco" value="Palco"> Palco
+            </div>
+            <div class="form-group">
+               <input type="checkbox" name="items[]" id="Cerveja Grátis" value="Cerveja Grátis"> Cerveja Grátis
+            </div>
+            <div class="form-group">
+               <input type="checkbox" name="items[]" id="Open food" value="Open food"> Open food
+            </div>
+            <div class="form-group">
+               <input type="checkbox" name="items[]" id="Brindes" value="Brindes"> Brindes
+            </div> 
          </div>
          <input type="submit" class="btn btn-primary" value="Criar evento">
       </form>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
             <div class="card col-md-3">
                 <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                 <div class="card-body">
-                    <p class="card-date">10/09/2022</p>
+                    <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
                     <h5 class="card-title">{{ $event->title }}</h5>
                     <p class="card-participants">X participantes</p>
                     <a href="/eventos/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
