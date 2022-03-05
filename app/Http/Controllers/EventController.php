78120ffa0ee8c1b9b261b18979ee24a3e95e45de
Michael Vinicius <michaelvini62@gmail.com>
2022-03-05 16:57:00 -0300

    add items and date to event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -23,6 +23,8 @@ class EventController extends Controller
         $event->description = $request->description;
         $event->city = $request->city;
         $event->private = $request->private;
+        $event->items = $request->items;
+        $event->date = $request->date;
 
         if($request->hasFile('image') && $request->file('image')->isValid()) {
             
