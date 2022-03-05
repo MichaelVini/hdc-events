03f153de3b10602f2da63342582cb5926af1e8a2
Michael Vinicius <michaelvini62@gmail.com>
2022-03-05 18:08:23 -0300

    show available event by search

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,19 @@
 @section('content')
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
-        <input type="text" id="search" name="serch" class="form-control" placeholder="Pesquisar Evento">
+    <form action="/" method="GET">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Pesquisar Evento">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
     <h2>Pŕoximos Eventos</h2>
     <p>Veja os eventos dos próximos dias</p>
+    @if (count($events) === 0 && $search)
+        <p>Não foi possível encontrar eventos disponíveis para: {{ $search }}</p>
+        <a class="px-2" href="/">Voltar</a>
+    @elseif (count($events) === 0)
+        <p>Ainda não há eventos disponíveis</p>
+    @endif
     <div id="cards-container" class="row">
         @foreach ($events as $event)
             <div class="card col-md-3">
